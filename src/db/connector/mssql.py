63da74d849c366a507a75d46251dc1ff507eccfa
frehnrob <frehner.robin@gmail.com>
2021-03-29 18:02:51 +0200

    # mssql connection bugfix

--- src/db/connector/mssql.py
+++ src/db/connector/mssql.py
@@ -5,7 +5,6 @@ from sqlalchemy import create_engine
 
 def create_mssql_engine(cfg):
     params = urllib.parse.quote_plus(
-        r'DRIVER={ODBC Driver 13 for SQL Server};SERVER=MSSQLBENCH;DATABASE={};Trusted_Connection=yes'.format(
-            cfg['db']))
+        r'DRIVER={ODBC Driver 13 for SQL Server};SERVER=MSSQLBENCH;DATABASE=' + cfg['db'] + ';Trusted_Connection=yes')
     conn_sql_server = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
     return create_engine(conn_sql_server, fast_executemany=True)
--- src/db/setup/setup_factory.py
+++ src/db/setup/setup_factory.py
@@ -4,3 +4,5 @@ from db.setup.postgres_forceorder_setup import setup, teardown
 def get_setup_teardown(name, cfg):
     if name == 'postgres-default':
         return (lambda engine, schema: setup(engine, schema), lambda engine, schema: teardown(engine, schema))
+    else:
+        return lambda engine, schema: (), lambda engine, schema: ()
