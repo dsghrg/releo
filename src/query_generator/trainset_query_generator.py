2f286199490e33e2bd31dca6350aa106c7a15b65
dennis gehrig <gehride1@students.zhaw.ch>
2021-04-17 12:12:22 +0200

    removed warning

--- src/query_generator/trainset_query_generator.py
+++ src/query_generator/trainset_query_generator.py
@@ -69,7 +69,7 @@ class TrainsetQueryGenerator:
 
 
 def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    """powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"""
     s = list(iterable)
     return itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s) + 1))
 
