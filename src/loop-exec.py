bcd455aa9c64d8722e5392a80f39db0f0ac9196e
frehnrob <frehner.robin@gmail.com>
2021-05-24 17:32:43 +0200

    -- configs for hyperparam "search"

--- src/loop-exec.py
+++ src/loop-exec.py
@@ -1,7 +1,37 @@
 import os
+import sys
+import glob
+import yaml
+import shutil
+import random
+import math
 
 os.system('which python3')
+base_dir = cfg_file = sys.argv[1]
+
+files = glob.glob(base_dir + '/**.yaml')
+
 for i in range(0, 30):
-    print(str(i) + '\tstart executing main.py')
-    os.system('python3 main.py')
-    print(str(i) + '\tfinished executing main.py')
+    for filepath in files:
+        dest = filepath.replace('.yaml', '-tmp.yaml')
+        shutil.copy(filepath, dest)
+        filepath = dest
+        if 'epsilon-rnd' in filepath:
+            config = None
+            with open(filepath, 'r') as stream:
+                config = yaml.load(stream, Loader=yaml.FullLoader)
+            with open(filepath, 'w') as stream:
+                config['agent-config']['epsilon-random-seed'] = int(math.floor(random.random() * 1000))
+                yaml.dump(config, stream, Dumper=yaml.CDumper)
+        if 'query-gen-rnd' in filepath:
+            config = None
+            with open(filepath, 'r') as stream:
+                config = yaml.load(stream, Loader=yaml.FullLoader)
+            with open(filepath, 'w') as stream:
+                config['query-generator-config']['random-seed'] = int(math.floor(random.random() * 1000))
+                yaml.dump(config, stream, Dumper=yaml.CDumper)
+        print('starting for ' + filepath)
+        os.system('python3 main.py ' + filepath)
+        print('ended for ' + filepath)
+        os.remove(filepath)
+        print('removed ' + filepath)
