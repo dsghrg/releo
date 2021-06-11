715cd8fa65d86ef43528faa2e9bdd3d2981008a7
frehnrob <frehner.robin@gmail.com>
2021-06-11 16:27:58 +0200

    -- final final ba commit ..-

--- db/db_schema/schema_mssql.sql
+++ db/db_schema/schema_mssql.sql
@@ -48,3 +48,126 @@ CREATE TABLE dbo.subcategory (
     category_id bigint NOT NULL
 )
 
+--ALTER TABLE [dbo].[categor]
+-- ADD CONSTRAINT FK_Product_ProductCategoryID FOREIGN KEY (ProductCategoryID)
+   -- REFERENCES [dbo].[ProductCategory] (ProductCategoryID)
+
+--ALTER TABLE [dbo].category
+ --   ADD CONSTRAINT category_pkey PRIMARY KEY (id);
+
+
+ALTER TABLE  [dbo].customer
+    ADD CONSTRAINT customer_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2748 (class 2606 OID 63701)
+-- Name: deliverer deliverer_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE  [dbo].deliverer
+    ADD CONSTRAINT deliverer_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2750 (class 2606 OID 63714)
+-- Name: discount discount_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE  [dbo].discount
+    ADD CONSTRAINT discount_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2746 (class 2606 OID 63681)
+-- Name: order_details order_details_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE  [dbo].order_details
+    ADD CONSTRAINT order_details_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2744 (class 2606 OID 63669)
+-- Name: order order_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE  [dbo].[order]
+    ADD CONSTRAINT order_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2736 (class 2606 OID 63620)
+-- Name: product product_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE  [dbo].product
+    ADD CONSTRAINT product_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2740 (class 2606 OID 63642)
+-- Name: subcategory subcategory_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE  [dbo].subcategory
+    ADD CONSTRAINT subcategory_pkey PRIMARY KEY (id);
+
+
+
+
+
+--
+-- TOC entry 2755 (class 2606 OID 63670)
+-- Name: order fk_customer_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE  [dbo].[order]
+    ADD CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES [dbo].customer(id);
+
+
+--
+-- TOC entry 2756 (class 2606 OID 63702)
+-- Name: [order] fk_deliverer_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE  [dbo].[order]
+    ADD CONSTRAINT fk_deliverer_id FOREIGN KEY (deliverer_id) REFERENCES [dbo].deliverer(id);
+
+
+--
+-- TOC entry 2757 (class 2606 OID 63717)
+-- Name: [order] fk_discount_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE  [dbo].[order]
+    ADD CONSTRAINT fk_discount_id FOREIGN KEY (discount_id) REFERENCES [dbo].discount(id);
+
+
+--
+-- TOC entry 2758 (class 2606 OID 63682)
+-- Name: [order]_details fk_[order]_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE  [dbo].order_details
+    ADD CONSTRAINT fk_order_id FOREIGN KEY (order_id) REFERENCES [dbo].[order](id);
+
+
+--
+-- TOC entry 2759 (class 2606 OID 63687)
+-- Name: [order]_details fk_product_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE  [dbo].order_details
+    ADD CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES [dbo].product(id);
+
+
+--
+-- TOC entry 2753 (class 2606 OID 63648)
+-- Name: product fk_subcategory_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE  [dbo].product
+    ADD CONSTRAINT fk_subcategory_id FOREIGN KEY (subcategory_id) REFERENCES [dbo].subcategory(id);
+
+
--- db/insert_scripts/create_non_uniform.py
+++ db/insert_scripts/create_non_uniform.py
@@ -1,23 +1,22 @@
 import math
 import random
 import string
+
 import pandas as pd
-from sqlalchemy import create_engine
-from sqlalchemy.schema import DropTable
-from sqlalchemy.ext.compiler import compiles
-import urllib
 from dbconnection import postgres_connection
+from sqlalchemy import create_engine
+from sqlalchemy.ext.compiler import compiles
+from sqlalchemy.schema import DropTable
+import urllib
 
-# from dbconnection import postgres_connection
-
-size_customer = int(500000 * 1)
+size_customer = 500000
 size_deliverer = 4
-size_discount = 50 * 1
-size_category = 1000 * 1
-size_subcategory = 5000 * 1
-size_product = 100000 * 1
-size_order = 2000000 * 1
-size_order_details = 6000000 * 1
+size_discount = 50
+size_category = 1000
+size_subcategory = 5000
+size_product = 100000
+size_order = 2000000
+size_order_details = 6000000
 
 
 @compiles(DropTable, "postgresql")
@@ -29,30 +28,31 @@ def random_string_generator(str_size, allowed_chars):
     return ''.join(random.choice(allowed_chars) for x in range(str_size))
 
 
-print('creating pandas tables...')
-# Customer table
+random_string_generator(30, string.ascii_letters)
+
+random.seed(1)
 customer_lst = [[k, random_string_generator(30, string.ascii_letters)] for k in range(1, size_customer + 1)]
 customer_df = pd.DataFrame(customer_lst, columns=['id', 'name'])
 
-# deliverer table
+random.seed(5)
 deliverer_lst = [[1, 'DHL'], [2, 'IPS'], [3, 'FEDEX'], [4, 'UPS']]
 deliverer_df = pd.DataFrame(deliverer_lst, columns=['id', 'name'])
 
-# discount table
+random.seed(100)
 discount_lst = [[k, random_string_generator(30, string.ascii_letters), random.uniform(0, 1)] for k in
                 range(1, size_discount + 1)]
 discount_df = pd.DataFrame(discount_lst, columns=['id', 'code', 'discount'])
 
-# category table
-category_lst = [[k, random_string_generator(30, string.ascii_letters)] for k in range(1, size_category + 1)]
+random.seed(1000)
+category_lst = [[k + 9990, random_string_generator(30, string.ascii_letters)] for k in range(1, size_category + 1)]
 category_df = pd.DataFrame(category_lst, columns=['id', 'category_name'])
 
-# subcategory table
+random.seed(12)
 subcategory_lst = [[k, random_string_generator(30, string.ascii_letters), random.randint(1, size_category)] for k in
                    range(1, size_subcategory + 1)]
 subcategory_df = pd.DataFrame(subcategory_lst, columns=['id', 'subcategory_name', 'category_id'])
 
-# order table
+random.seed(15)
 order_lst = [[k,
               random.randint(1, size_customer),
               random.uniform(0, 1) * 1480 + 20,
@@ -60,14 +60,14 @@ order_lst = [[k,
               random.randint(1, size_discount)] for k in range(1, size_order + 1)]
 order_df = pd.DataFrame(order_lst, columns=['id', 'customer_id', 'amount', 'deliverer_id', 'discount_id'])
 
-# product table
+random.seed(117)
 product_lst = [[k,
                 random_string_generator(30, string.ascii_letters),
                 math.floor(random.uniform(0, 1) * 480 + 20),
                 random.randint(1, size_subcategory)] for k in range(1, size_product + 1)]
 product_df = pd.DataFrame(product_lst, columns=['id', 'name', 'price', 'subcategory_id'])
 
-# order_details table
+random.seed(199)
 order_details_lst = [[k,
                       random.randint(1, size_order),
                       random.randint(1, size_product),
@@ -75,51 +75,28 @@ order_details_lst = [[k,
                       random.randint(1, 20)] for k in range(1, size_order_details + 1)]
 order_details_df = pd.DataFrame(order_details_lst, columns=['id', 'order_id', 'product_id', 'price', 'quantity'])
 
+SERVERNAME = 'mssqlbench.cloudlab.zhaw.ch'
+SERVER = 'MSSQLBENCH'
+DATABASE = 'random_database_small'
+
 print('connecting to db...')
+
+params_log = urllib.parse.quote_plus(r'DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=order_db_non_unif;Trusted_Connection=yes')
+conn_sql_server = 'mssql+pyodbc:///?odbc_connect={}'.format(params_log)
+engine = create_engine(conn_sql_server, fast_executemany = True)
+#
 # creds = postgres_connection()
 # engine = create_engine(
 #     'postgresql://{}:{}@{}:5432/{}'.format(creds['user'], creds['password'],
 #                                            creds['host'], creds['database']))
-
-# SERVERNAME = 'mssqlbench.cloudlab.zhaw.ch'
-# SERVER = 'MSSQLBENCH'
-# DATABASE = 'random_database_small'
-
-# print('connected!')
-#
-# params_log = urllib.parse.quote_plus(r'DRIVER={ODBC Driver 13 for SQL Server};SERVER=MSSQLBENCH;DATABASE=random_database_small;Trusted_Connection=yes')
-# conn_sql_server = 'mssql+pyodbc:///?odbc_connect={}'.format(params_log)
-#
-# engine = create_engine(postgres_engine, fast_executemany=True)
-
-
-# SERVERNAME = 'localhost'
-# SERVER = 'MSSQLSERVER'
-# DATABASE = 'shop_db'
-#
-# params_log = urllib.parse.quote_plus(r'DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=shop_db;Trusted_Connection=yes')
-# conn_sql_server = 'mssql+pyodbc:///?odbc_connect={}'.format(params_log)
-# engine = create_engine(conn_sql_server, fast_executemany = True)
-
-params = urllib.parse.quote_plus(
-    r'DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost,5432;DATABASE=shop_db;PWD=zhaw2021_mssql;UID=SA')
-conn_sql_server = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
-engine = create_engine(conn_sql_server, fast_executemany=True)
 print('connected')
 
 customer_df.to_sql("customer", con=engine, schema='public', if_exists='replace', index=False)
-print("Customer done")
 deliverer_df.to_sql("deliverer", con=engine, schema='public', if_exists='replace', index=False)
-print("deliverer done")
 discount_df.to_sql("discount", con=engine, schema='public', if_exists='replace', index=False)
-print("discount done")
-category_df.to_sql("category", con=engine, schema='public', if_exists='replace', index=False)
-print("category done")
 subcategory_df.to_sql("subcategory", con=engine, schema='public', if_exists='replace', index=False)
-print("subcategory done")
 order_df.to_sql("order", con=engine, schema='public', if_exists='replace', index=False)
-print("order done")
 product_df.to_sql("product", con=engine, schema='public', if_exists='replace', index=False)
-print("product done")
 order_details_df.to_sql("order_details", con=engine, schema='public', if_exists='replace', index=False)
-print("order_details done")
+
+category_df.to_sql("category", con=engine, schema='public', if_exists='replace', index=False)
